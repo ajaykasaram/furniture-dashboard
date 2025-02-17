0d42628e2c1fd55a19fd5a6f58ca68cb4d8fd49e
ajaykasaram <ajay.kasaram@outlook.com>
2025-02-17 12:03:16 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,4 +10,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 3000,
+    host: true // needed for the Docker Container port mapping to work
+  },
+  preview: {
+    port: 3000,
+    host: true
+  }
 })
